c7af8ccd816571613d66be1f99e7767f7295b6e3
elahe01~ <elahe.narimani@gmail.com>
2023-10-25 17:09:31 +0330

    project complete with js

--- script.js
+++ script.js
@@ -8,76 +8,117 @@ let food=[{foodName:'همبرگر مخصوص ', cost:10000 , number:0, sumCost:0
           {foodName:'همبرگر مخصوص با قارچ و پنیر ', cost:20000 , number:0, sumCost:0 , id:2},
           {foodName:'همبرگر معمولی با قارچ و پنیر ', cost:10000 , number:0, sumCost:0 , id:3},
           {foodName:'سیب زمینی سرخ کرده ', cost:10000 , number:0, sumCost:0 , id:4},
-          {foodName: 'سیب زمینی سرخ کرده ویژه', cost:25000 , number:0, sumCost:0 , id:5},
+          {foodName:'سیب زمینی سرخ کرده ویژه', cost:25000 , number:0, sumCost:0 , id:5},
           {foodName:'نوشابه ', cost:5000 , number:0, sumCost:0 , id:6},
           {foodName:'نوشابه رژیمی ', cost:6000 , number:0, sumCost:0 , id:7},
           {foodName:'سالاد سزار ', cost:25000 , number:0, sumCost:0 , id:8},
           {foodName:'سالاد فصل ', cost:8000 , number:0, sumCost:0 , id:9},
          ];
 //start add
-function addAmount(amount , foodId){//مقدار قبلی را میگیرد
-        let addedAmount= amount +1;
-        refreshData(foodId , addedAmount);  
-}
 
 function addBtnClickHandler(foodId){ 
-    const finder = food.find(item => item.id == foodId) ;
+    // console.log("test")
+    const finder = food.find(item => item.id == foodId) ;//find one object
+    // console.log(finder)
     if (finder){
-        addAmount(finder.number , finder.id);
-    }  
+        addAmount(finder.number , finder.id);//pass this id and number to this function
+    }     
 }
-//end add
-function refreshData(foodIdentifire , changedUnit){
+function addAmount(amount , foodId){//مقدار قبلی را میگیرد
+    let addedAmount = amount + 1;
+    console.log(addedAmount)
     console.log(food)
     food = food.map(item => {
-        if(item.id == foodIdentifire){
-            console.log(item.number);
-            if(item.number == 0){
-                totalcost = item.cost;
-            }else{
-                totalcost = (item.cost * (item.number + 1))
+        if(item.id == foodId){
+            return {
+                foodName : item.foodName,
+                cost : item.cost,
+                number : addedAmount,
+                sumCost : item.sumCost,
+                id : item.id
             }
-           return{
-            foodName : item.foodName ,
-            cost : item.cost ,
-            number : changedUnit ,
-            sumCost : totalcost ,
-            id : item.id 
-           }   
         }else{
             return{
-                foodName : item.foodName ,
-                cost : item.cost ,
-                number : item.number ,
-                sumCost : item.sumCost ,
+                foodName : item.foodName,
+                cost : item.cost,
+                number : item.number,
+                sumCost : item.sumCost,
                 id : item.id
-            }   
-        }
-       
-    });
-    makeFoodInformation();
-    totalCostItem(changedUnit)
-    console.log(food);
+            }
+        } 
+})
+console.log(food);
+makeFoodInformation();
+totalcostItem(foodId);
+    // refreshData(foodId , addedAmount);  
 }
-
+function totalcostItem(foodId){
+    console.log(food)
+    food = food.map(item => {
+        let totalItem = (item.cost * item.number);
+        if(item.id == foodId){
+            return {
+                foodName : item.foodName,
+                cost : item.cost,
+                number : item.number,
+                sumCost : totalItem,
+                id : item.id
+            }
+        }else{
+            return{
+                foodName : item.foodName,
+                cost : item.cost,
+                number : item.number,
+                sumCost : item.sumCost,
+                id : item.id
+            }
+        } 
+})
+console.log(food);
+makeFoodInformation();
+}
+//end add
 //start reduce{
+function reduceBtnClickHandler(foodId){ 
+     const finder = food.find(item => item.id == foodId) ;
+    if (finder){
+         reduceAmount(finder.number , finder.id);
+    }   
+}    
 function reduceAmount(amount , foodId){//مقدار را بخوان یکی ازش کم کن
     let reducedAmount = amount - 1;
-    console.log(reducedAmount);
     if(reducedAmount >= 0){
-        refreshData(foodId , reducedAmount);
+        food = food.map(item => {
+            if(item.id == foodId){
+                return {
+                    foodName : item.foodName,
+                    cost : item.cost,
+                    number : reducedAmount,
+                    sumCost : item.sumCost,
+                    id : item.id
+                }
+            }else{
+                return{
+                    foodName : item.foodName,
+                    cost : item.cost,
+                    number : item.number,
+                    sumCost : item.sumCost,
+                    id : item.id
+                }
+            } 
+    })
     }else{
         reducedAmount = 0;
         console.log(reducedAmount);
-    }    
+    }
+    console.log(reducedAmount)
+    console.log(food)
+    
+console.log(food);
+makeFoodInformation();
+totalcostItem(foodId);       
 }
-function reduceBtnClickHandler(foodId){ 
-    const finder = food.find(item => item.id == foodId) ;
-    if (finder){
-        reduceAmount(finder.number , finder.id);
-    }   
-}
-//end reduce}
+    //end reduce}
 function makeFoodInformation(){
     let information = "";
     food.forEach(item => {
@@ -128,6 +169,70 @@ function makeFoodInformation(){
     foodItem.innerHTML= information;
 }
 makeFoodInformation();
-function makeTotalCostInformation(){
-    
+let pay = [{totalCostPay : totalCostPay , tip : tip , off : off , amountPayable : amountPayable}]
+function makePayTotalCost(){
+    let PayInformation = "";
+    pay.forEach(item => {
+         PayInformation = PayInformation +`
+         <div
+                class="h-[349px] w-full bg-[#F0F8FF] inline-block  2xl:w-[349px] 2xl:absolute 2xl:left-[80px] 2xl:top-[30px] p-[15px] rounded-[20px]">
+                <div class=" flex flex-col justify-start gap-[10px] ">
+                    <div class="flex justify-between items-center">
+                        <p>جمع کل سفارشات:</p>
+                        <p>${item.totalCostPay} تومان</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <p>حق سرویس و کارمزد:</p>
+                        <p>${item.tip} تومان</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <p>تخفیف:</p>
+                        <p>${item.off} تومان</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <input placeholder="کد تخفیف" class="w-[100%] h-[40px] pr-[5px] pl-0 ml-0">
+                        <button
+                            class="w-[50px] h-[40px] bg-[#9E1010] rounded-tl-[5px] rounded-bl-[5px] pr-0 mr-0 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="white"
+                                class="bi bi-check-lg " viewBox="0 0 16 16">
+                                <path
+                                    d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="w-[100%]  flex flex-col mt-[60px] gap-[10px] ">
+                    <div class="flex justify-between items-center bg-[#FFC72C] h-[35px]">
+                        <p>مبلغ قابل پرداخت:</p>
+                        <p>${item.amountPayable} تومان</p>
+                    </div>
+                    <div class="h-[50px] text-center bg-[#D1D5DB] py-[10px]">
+                        <button>ثبت سفارش</button>
+                    </div>
+                </div>
+            </div>
+         `
+    })
 }
+function addSumCost(){
+        // let result = 0;
+    for (let item of food){
+        console.log(food)
+        //   result = item.sumCost
+    }
+        
+}
+addSumCost()
+ makePayTotalCost();
+// function refreshOrder(sumOrder){
+//     pay = pay.map(item => {
+//         let sumOrder = 
+//         return {
+//             totalCostPay : sumOrder,
+//             tip : item.tip,
+//             off : item.off,
+//             amountPayable : item.amountPayable
+//         }
+//     })
+//     makePayTotalCost() ;
+// } 
